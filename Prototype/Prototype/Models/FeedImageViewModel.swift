e7069bd3af6727a124c058ad83ba30182905be97
Dennis Yaremenko <yaremdennis@gmail.com>
2024-10-21 10:33:11 +0300

    Add prototype feed data for demoing purposes

---
+++ Prototype/Prototype/Models/FeedImageViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedImageViewModel.swift
+//  Prototype
+//
+//  Created by Denis Yaremenko on 21.10.2024.
+//
+
+
+struct FeedImageViewModel {
+     let description: String?
+     let location: String?
+     let imageName: String
+ }
